131b49424defa4f9a5a9dd43ba32dc669d18ae36
Gabriel Cabeda <gabrielcabeda@hotmail.com>
2023-02-20 22:23:21 -0300

    corrección del comentario

--- array.js
+++ array.js
@@ -12,5 +12,5 @@ for (i=0; i<15; i++) {
 console.log(" 15 números aleatorios: "+ lista)
 
 
-//adentro del del for me va mostrando cada vuelta y suma un numero mas cada vez,
-//afuera del for primero recorre las 15 veces y luego imprime en pantalla la lista final
+//dentro del del `for` me va mostrando cada vuelta y suma un numero más cada vez,
+//afuera del `for` primero recorre las 15 veces y luego imprime en pantalla la lista final
